21e5e695352da45afd5aacd90acec0557986e566
Matboud <m.matboud@gmail.com>
2021-04-29 17:41:41 +0000

    [91] Resource Reading Endpoints 1

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,6 +21,30 @@ app.post("/users", (req, res) => {
     });
 });
 
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((err) => {
+      res.status(500).send();
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch(() => {
+      res.status(500).send();
+    });
+});
+
 app.post("/tasks", (req, res) => {
   const task = new Task(req.body);
 
