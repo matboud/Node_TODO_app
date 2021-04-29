6ae8362737362ae5d0997aad83d71009ced38d72
Matboud <m.matboud@gmail.com>
2021-04-29 18:54:32 +0000

    [94] promise chaining challenge

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -5,5 +5,5 @@ const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api";
 mongoose.connect(connectionURL, {
   useNewUrlParser: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 });
-
