cc2e6669c42987a5d816a3a9c2e513024bcf56b8
Matboud <m.matboud@gmail.com>
2021-04-29 16:40:18 +0000

    files architecture, and CREATE user, task

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api";
 
@@ -8,79 +7,3 @@ mongoose.connect(connectionURL, {
   useCreateIndex: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes("password")) {
-        throw new Error('Password cannot contain "password"');
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    },
-  },
-});
-
-// const me = new User({
-//   name: "  User1 ",
-//   email: "user1@gmail.com ",
-//   password: "myPass1234",
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.log("Unable to sqve User model:", error);
-//   });
-
-const Task = mongoose.model("Task", {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-const newTask = new Task({
-});
-
-newTask
-  .save()
-  .then(() => {
-    console.log(newTask);
-  })
-  .catch((error) => {
-    console.log("Unable to sqve User model:", error);
-  });
