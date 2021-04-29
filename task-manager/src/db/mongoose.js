0bf1009bf2f4c4aa54b9d6a566042cbd8fdcf5de
Matboud <m.matboud@gmail.com>
2021-04-29 01:38:49 +0000

    installing mongoose, creatig Users model using mongoose

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const connectionURL = "mongodb://127.0.0.1:27017/task-manager-api";
+
+mongoose.connect(connectionURL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+const me = new User({
+  name: "Matboud",
+  age: 26,
+});
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.log("Unable to sqve User model:", error);
+  });
