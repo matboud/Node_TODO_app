e9e1013f73c9652923c2bfbb8b0f54f4e2be9e45
Matboud <m.matboud@gmail.com>
2021-04-30 22:14:40 +0000

    [106] JSON web tokens

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,3 +14,18 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server ON on port: ", port);
 });
+
+const jwt = require("jsonwebtoken");
+
+const func = async () => {
+  const token = jwt.sign({ _id: "abcd123" }, "creatingacooltaskmanagerapp", {
+    expiresIn: "7 days",
+  });
+  // console.log(token);
+
+  const data = jwt.verify(token, "creatingacooltaskmanagerapp");
+
+  console.log("====>", data);
+};
+
+func();
