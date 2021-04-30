9881b9101d572598f8c97aa81a5417f9c5f5b225
Matboud <m.matboud@gmail.com>
2021-04-30 01:07:28 +0000

    [99] update tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -104,6 +104,32 @@ app.get("/tasks/:id", async (req, res) => {
   }
 });
 
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "invalid updates!" });
+  }
+
+  try {
+    const user = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    return res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 app.listen(port, () => {
   console.log("Server ON on port: ", port);
 });
