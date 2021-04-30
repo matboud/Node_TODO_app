d0ac1c10bbadda7014f9ecfae024d62941944787
Matboud <m.matboud@gmail.com>
2021-04-30 02:12:33 +0000

    [101] separate route files

---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const router = new express.Router();
+const Task = require("../models/task");
+
+router.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "invalid updates!" });
+  }
+
+  try {
+    const user = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    return res.send(user);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+router.delete("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findByIdAndDelete(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = router;
