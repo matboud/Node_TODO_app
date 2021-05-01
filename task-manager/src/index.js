3fddec7a3b05560a216eea0a5bd84abbb6a1c73e
Matboud <m.matboud@gmail.com>
2021-05-01 18:51:43 +0000

    [109] middleware accept auth token, logged in user profile route

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 require("./db/mongoose");
 
+
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use((req, res, next) => {
-  res.status(503).send("Service is currently DOWN, check back soon!");
-});
 
 app.use(express.json());
 app.use(userRouter);
