5cfdf4e6e6724f176459163a0afe772c9acf1dcf
Matboud <m.matboud@gmail.com>
2021-05-01 17:04:17 +0000

    [107, 108] generating auth Token for sign-up and login & exploring express middlewares

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,10 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.status(503).send("Service is currently DOWN, check back soon!");
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -6,8 +6,9 @@ router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
+    const token = await user.generateAuthToken();
     await user.save();
-    res.status(201).send(user);
+    res.status(201).send({ user, token });
   } catch (err) {
     res.status(400).send(err);
   }
@@ -19,7 +20,8 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
