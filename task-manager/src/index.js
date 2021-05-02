d662633f4e4d7130a7c880440c3c76230c2f3500
Matboud <m.matboud@gmail.com>
2021-05-02 06:08:23 +0000

    [116] on delete account == delete all related tasks of the user

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,18 +14,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server ON on port: ", port);
 });
-
-const Task = require("./models/task");
-const User = require("./models/user");
-
-const main = async () => {
-  // const task = await Task.findById("608e30bd25dad311e8ce0c41");
-  // await task.populate("owner").execPopulate()
-
-  const user = await User.findById('608e30a825dad311e8ce0c3f');
-  await user.populate('tasks').execPopulate();
-
-  console.log(user.tasks);
-};
-
-main();
